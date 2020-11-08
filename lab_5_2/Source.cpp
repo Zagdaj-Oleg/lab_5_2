dae8bbd356bc78b9ef0727e1544979baf7f8a9af
Zagdaj-Oleg <72668217+Zagdaj-Oleg@users.noreply.github.com>
2020-11-08 11:36:16 +0200

    lab_5_2

---
+++ lab_5_2/Source.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std; 
+double Pi;
+double S(const double x, const double eps, int& n, double s);
+double A(const double x, const int n, double a);
+int main()
+{
+	double xp, xk, x, dx, eps, s = 0;
+	int n = 0;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "exp(x)" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		s = S(x, eps, n, s);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << exp(x) << " |"
+			<< setw(10) << setprecision(5) << s << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
+double S(const double x, const double eps, int& n, double s)
+{
+	Pi = 4 * atan(1.0);
+	n = 0; 
+	double a = Pi/2; 
+	s = a;
+	do {
+		n++;
+			a = A(x, n, a);
+		s += a;
+	} while (abs(a) >= eps);
+	return s;
+}
+double A(const double x, const int n, double a)
+{
+	double R = 1 / n * pow(x, n);  
+	a *= R;
+	return a;
+}
